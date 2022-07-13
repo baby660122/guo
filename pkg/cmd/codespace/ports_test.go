4794ad6019b91a1b9ee6a1e8748a55093d0187cd
baby660122 <99910306+baby660122@users.noreply.github.com>
2022-07-13 08:06:56 +0800

    Initialize jg/event-handling-deux

---
+++ pkg/cmd/codespace/ports_test.go
@@ -0,0 +1,234 @@
+package codespace
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/cli/cli/v2/internal/codespaces/api"
+	"github.com/cli/cli/v2/pkg/iostreams"
+	"github.com/cli/cli/v2/pkg/liveshare"
+	livesharetest "github.com/cli/cli/v2/pkg/liveshare/test"
+	"github.com/sourcegraph/jsonrpc2"
+)
+
+func TestPortsUpdateVisibilitySuccess(t *testing.T) {
+	portVisibilities := []portVisibility{
+		{
+			number:     80,
+			visibility: "org",
+		},
+		{
+			number:     9999,
+			visibility: "public",
+		},
+	}
+
+	eventResponses := []string{
+		"serverSharing.sharingSucceeded",
+		"serverSharing.sharingSucceeded",
+	}
+
+	portsData := []liveshare.PortNotification{
+		{
+			Success: true,
+			PortUpdate: liveshare.PortUpdate{
+				Port:       80,
+				ChangeKind: liveshare.PortChangeKindUpdate,
+			},
+		},
+		{
+			Success: true,
+			PortUpdate: liveshare.PortUpdate{
+				Port:       9999,
+				ChangeKind: liveshare.PortChangeKindUpdate,
+			},
+		},
+	}
+
+	err := runUpdateVisibilityTest(portVisibilities, eventResponses, portsData)
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+func TestPortsUpdateVisibilityFailure403(t *testing.T) {
+	portVisibilities := []portVisibility{
+		{
+			number:     80,
+			visibility: "org",
+		},
+		{
+			number:     9999,
+			visibility: "public",
+		},
+	}
+
+	eventResponses := []string{
+		"serverSharing.sharingSucceeded",
+		"serverSharing.sharingFailed",
+	}
+
+	portsData := []liveshare.PortNotification{
+		{
+			Success: true,
+			PortUpdate: liveshare.PortUpdate{
+				Port:       80,
+				ChangeKind: liveshare.PortChangeKindUpdate,
+			},
+		},
+		{
+			Success: false,
+			PortUpdate: liveshare.PortUpdate{
+				Port:        9999,
+				ChangeKind:  liveshare.PortChangeKindUpdate,
+				ErrorDetail: "test error",
+				StatusCode:  403,
+			},
+		},
+	}
+
+	err := runUpdateVisibilityTest(portVisibilities, eventResponses, portsData)
+	if err == nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if errors.Unwrap(err) != errUpdatePortVisibilityForbidden {
+		t.Errorf("expected: %v, got: %v", errUpdatePortVisibilityForbidden, errors.Unwrap(err))
+	}
+}
+
+func TestPortsUpdateVisibilityFailure(t *testing.T) {
+	portVisibilities := []portVisibility{
+		{
+			number:     80,
+			visibility: "org",
+		},
+		{
+			number:     9999,
+			visibility: "public",
+		},
+	}
+
+	eventResponses := []string{
+		"serverSharing.sharingSucceeded",
+		"serverSharing.sharingFailed",
+	}
+
+	portsData := []liveshare.PortNotification{
+		{
+			Success: true,
+			PortUpdate: liveshare.PortUpdate{
+				Port:       80,
+				ChangeKind: liveshare.PortChangeKindUpdate,
+			},
+		},
+		{
+			Success: false,
+			PortUpdate: liveshare.PortUpdate{
+				Port:        9999,
+				ChangeKind:  liveshare.PortChangeKindUpdate,
+				ErrorDetail: "test error",
+			},
+		},
+	}
+
+	err := runUpdateVisibilityTest(portVisibilities, eventResponses, portsData)
+	if err == nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	var expectedErr *ErrUpdatingPortVisibility
+	if !errors.As(err, &expectedErr) {
+		t.Errorf("expected: %v, got: %v", expectedErr, err)
+	}
+}
+
+type joinWorkspaceResult struct {
+	SessionNumber int `json:"sessionNumber"`
+}
+
+func runUpdateVisibilityTest(portVisibilities []portVisibility, eventResponses []string, portsData []liveshare.PortNotification) error {
+	joinWorkspace := func(req *jsonrpc2.Request) (interface{}, error) {
+		return joinWorkspaceResult{1}, nil
+	}
+	const sessionToken = "session-token"
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ch := make(chan float64, 1)
+	updateSharedVisibility := func(rpcReq *jsonrpc2.Request) (interface{}, error) {
+		var req []interface{}
+		if err := json.Unmarshal(*rpcReq.Params, &req); err != nil {
+			return nil, fmt.Errorf("unmarshal req: %w", err)
+		}
+
+		ch <- req[0].(float64)
+		return nil, nil
+	}
+	testServer, err := livesharetest.NewServer(
+		livesharetest.WithNonSecure(),
+		livesharetest.WithPassword(sessionToken),
+		livesharetest.WithService("workspace.joinWorkspace", joinWorkspace),
+		livesharetest.WithService("serverSharing.updateSharedServerPrivacy", updateSharedVisibility),
+	)
+	if err != nil {
+		return fmt.Errorf("unable to create test server: %w", err)
+	}
+
+	type rpcMessage struct {
+		Method string
+		Params liveshare.PortUpdate
+	}
+
+	go func() {
+		var i int
+		for ; ; i++ {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ch:
+				pd := portsData[i]
+				testServer.WriteToObjectStream(rpcMessage{
+					Method: eventResponses[i],
+					Params: pd.PortUpdate,
+				})
+			}
+		}
+	}()
+
+	mockApi := &apiClientMock{
+		GetCodespaceFunc: func(ctx context.Context, codespaceName string, includeConnection bool) (*api.Codespace, error) {
+			return &api.Codespace{
+				Name:  "codespace-name",
+				State: api.CodespaceStateAvailable,
+				Connection: api.CodespaceConnection{
+					SessionID:      "session-id",
+					SessionToken:   sessionToken,
+					RelayEndpoint:  testServer.URL(),
+					RelaySAS:       "relay-sas",
+					HostPublicKeys: []string{livesharetest.SSHPublicKey},
+				},
+			}, nil
+		},
+	}
+
+	io, _, _, _ := iostreams.Test()
+	a := &App{
+		io:        io,
+		apiClient: mockApi,
+	}
+
+	var portArgs []string
+	for _, pv := range portVisibilities {
+		portArgs = append(portArgs, fmt.Sprintf("%d:%s", pv.number, pv.visibility))
+	}
+
+	err = a.UpdatePortVisibility(ctx, "codespace-name", portArgs)
+
+	return err
+}
---
+++ pkg/liveshare/rpc.go
@@ -0,0 +1,78 @@
+package liveshare
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"sync"
+	"time"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/sourcegraph/jsonrpc2"
+)
+
+type rpcClient struct {
+	*jsonrpc2.Conn
+	conn           io.ReadWriteCloser
+	requestHandler *requestHandler
+}
+
+func newRPCClient(conn io.ReadWriteCloser) *rpcClient {
+	return &rpcClient{conn: conn, requestHandler: newRequestHandler()}
+}
+
+func (r *rpcClient) connect(ctx context.Context) {
+	stream := jsonrpc2.NewBufferedStream(r.conn, jsonrpc2.VSCodeObjectCodec{})
+	r.Conn = jsonrpc2.NewConn(ctx, stream, r.requestHandler)
+}
+
+func (r *rpcClient) do(ctx context.Context, method string, args, result interface{}) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, method)
+	defer span.Finish()
+
+	waiter, err := r.Conn.DispatchCall(ctx, method, args)
+	if err != nil {
+		return fmt.Errorf("error dispatching %q call: %w", method, err)
+	}
+
+	// timeout for waiter in case a connection cannot be made
+	waitCtx, cancel := context.WithTimeout(ctx, 2*time.Minute)
+	defer cancel()
+
+	return waiter.Wait(waitCtx, result)
+}
+
+type handlerFn func(conn *jsonrpc2.Conn, req *jsonrpc2.Request)
+
+type requestHandler struct {
+	handlersMu sync.RWMutex
+	handlers   map[string][]handlerFn
+}
+
+func newRequestHandler() *requestHandler {
+	return &requestHandler{handlers: make(map[string][]handlerFn)}
+}
+
+func (r *requestHandler) register(requestType string, handler handlerFn) {
+	r.handlersMu.Lock()
+	defer r.handlersMu.Unlock()
+
+	if _, ok := r.handlers[requestType]; !ok {
+		r.handlers[requestType] = []handlerFn{}
+	}
+
+	r.handlers[requestType] = append(r.handlers[requestType], handler)
+}
+
+func (r *requestHandler) handlerFn(requestType string) []handlerFn {
+	r.handlersMu.RLock()
+	defer r.handlersMu.RUnlock()
+
+	return r.handlers[requestType]
+}
+
+func (r *requestHandler) Handle(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.Request) {
+	for _, handler := range r.handlerFn(req.Method) {
+		go handler(conn, req)
+	}
+}
